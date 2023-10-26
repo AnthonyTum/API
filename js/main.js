0671c03409abc2c37f4d4a953952dfcb008ca6d9
AnthonyTum <146720457+AnthonyTum@users.noreply.github.com>
2023-10-26 15:42:14 +0200

    peaufinage

--- js/main.js
+++ js/main.js
@@ -1,235 +1,71 @@
-const createAttribute = (label, value) => {
-    const attribute = document.createElement('tr');
-
-    const attributeLabel = document.createElement('td');
-    attributeLabel.textContent = label;
-
-    const attributeValue = document.createElement('td');
-    attributeValue.textContent = value;
-
-    attribute.appendChild(attributeLabel);
-    attribute.appendChild(attributeValue);
-
-    return attribute;
-};
-
-const createPlayerCard = (playerData) => {
-    const playerCard = document.createElement('div');
-    playerCard.className = 'player-card';
-
-    const playerImage = document.createElement('img');
-    playerImage.src = playerData.photo;
-    playerImage.style.width = '150px';
-    playerCard.appendChild(playerImage);
-
-    const playerName = document.createElement('h2');
-    playerName.textContent = playerData.nom;
-    playerCard.appendChild(playerName);
-
-    const playerInfoTable = document.createElement('table');
-
-    const playerAge = createAttribute('Âge', playerData.age);
-    const playerTaille = createAttribute('Taille', playerData.taille);
-    const playerPoids = createAttribute('Poids', playerData.poids);
-    const playerNationalite = createAttribute('Nationalité', playerData.nationalite);
-    const playerPostes = createAttribute('Postes', playerData.postes.join(', '));
-
-    playerInfoTable.appendChild(playerAge);
-    playerInfoTable.appendChild(playerTaille);
-    playerInfoTable.appendChild(playerPoids);
-    playerInfoTable.appendChild(playerNationalite);
-    playerInfoTable.appendChild(playerPostes);
-
-    const technicalAttributes = document.createElement('table');
-    technicalAttributes.className = 'attribute-table';
-    Object.entries(playerData.technical).forEach(([label, value]) => {
-        const attribute = createAttribute(label, value);
-        technicalAttributes.appendChild(attribute);
-    });
-
-    const mentalAttributes = document.createElement('table');
-    mentalAttributes.className = 'attribute-table';
-    Object.entries(playerData.mental).forEach(([label, value]) => {
-        const attribute = createAttribute(label, value);
-        mentalAttributes.appendChild(attribute);
-    });
-
-    const physicalAttributes = document.createElement('table');
-    physicalAttributes.className = 'attribute-table';
-    Object.entries(playerData.physical).forEach(([label, value]) => {
-        const attribute = createAttribute(label, value);
-        physicalAttributes.appendChild(attribute);
-    });
-
-    playerCard.appendChild(playerInfoTable);
-    playerCard.appendChild(technicalAttributes);
-    playerCard.appendChild(mentalAttributes);
-    playerCard.appendChild(physicalAttributes);
-
-    return playerCard;
-};
-
-const playersData = [
-    {
-        "nom": "Endrick",
-        "age": 15,
-        "taille": "1m73",
-        "poids": "61kg",
-        "nationalite": "Brésil",
-        "postes": ["Buteur", "Ailier droit"],
-        "photo": "img/Endrick.png",
-        "technical": {
-            "Centres": 10,
-            "Contrôle de Balle": 13,
-            "Corners": 7,
-            "Coup Francs": 12,
-            "Dribbles": 15,
-            "Finition": 13,
-            "Jeu de Tête": 9,
-            "Marquage": 4,
-            "Passes": 11,
-            "Penalty": 11,
-            "Tacles": 4,
-            "Technique": 13,
-            "Tirs de Loin": 10,
-            "Touches Longues": 2
-        },
-        "mental": {
-            "Agressivité": 11,
-            "Anticipation": 12,
-            "Appels de Balle": 12,
-            "Concentration": 11,
-            "Courage": 11,
-            "Décisions": 12,
-            "Détermination": 14,
-            "Inspiration": 18,
-            "Jeu Collectif": 9,
-            "Leadership": 3,
-            "Placement": 6,
-            "Sang-Froid": 13,
-            "Vision du Jeu": 11,
-            "Volume de Jeu": 11
-        },
-        "physical": {
-            "Accélération": 15,
-            "Agilité": 13,
-            "Détente Verticale": 6,
-            "Endurance": 11,
-            "Équilibre": 14,
-            "Puissance": 13,
-            "Qualité Physiques Naturelles": 14,
-            "Vitesse": 13
-        }
-    },
-    {
-        "nom": "Jamal Musiala",
-        "age": 19,
-        "taille": "1m83",
-        "poids": "70kg",
-        "nationalite": "Allemagne",
-        "postes": ["Ailier gauche", "Milieu Offensif Centre"],
-        "photo": "img/Musiala.png",
-        "technical": {
-            "Centres": 9,
-            "Contrôle de Balle": 16,
-            "Corners": 6,
-            "Coup Francs": 6,
-            "Dribbles": 19,
-            "Finition": 13,
-            "Jeu de Tête": 8,
-            "Marquage": 10,
-            "Passes": 15,
-            "Penalty": 12,
-            "Tacles": 12,
-            "Technique": 18,
-            "Tirs de Loin": 13,
-            "Touches Longues": 6
-        },
-        "mental": {
-            "Agressivité": 12,
-            "Anticipation": 14,
-            "Appels de Balle": 15,
-            "Concentration": 12,
-            "Courage": 12,
-            "Décisions": 14,
-            "Détermination": 17,
-            "Inspiration": 18,
-            "Jeu Collectif": 14,
-            "Leadership": 6,
-            "Placement": 10,
-            "Sang-Froid": 14,
-            "Vision du Jeu": 15,
-            "Volume de Jeu": 15
-        },
-        "physical": {
-            "Accélération": 15,
-            "Agilité": 17,
-            "Détente Verticale": 10,
-            "Endurance": 12,
-            "Équilibre": 16,
-            "Puissance": 10,
-            "Qualité Physiques Naturelles": 16,
-            "Vitesse": 15
-        }
-    },
-    {
-        "nom": "Bukayo Saka",
-        "age": 20,
-        "taille": "1m78",
-        "poids": "69kg",
-        "nationalite": "Angleterre",
-        "postes": ["Ailier droit", "Ailier gauche"],
-        "photo": "img/Saka.png",
-        "technical": {
-            "Centres": 15,
-            "Contrôle de Balle": 15,
-            "Corners": 13,
-            "Coup Francs": 9,
-            "Dribbles": 15,
-            "Finition": 13,
-            "Jeu de Tête": 8,
-            "Marquage": 10,
-            "Passes": 14,
-            "Penalty": 14,
-            "Tacles": 10,
-            "Technique": 16,
-            "Tirs de Loin": 13,
-            "Touches Longues": 6
-        },
-        "mental": {
-            "Agressivité": 12,
-            "Anticipation": 16,
-            "Appels de Balle": 16,
-            "Concentration": 14,
-            "Courage": 12,
-            "Décisions": 16,
-            "Détermination": 16,
-            "Inspiration": 16,
-            "Jeu Collectif": 17,
-            "Leadership": 9,
-            "Placement": 11,
-            "Sang-Froid": 15,
-            "Vision du Jeu": 16,
-            "Volume de Jeu": 15
-        },
-        "physical": {
-            "Accélération": 16,
-            "Agilité": 16,
-            "Détente Verticale": 9,
-            "Endurance": 16,
-            "Équilibre": 17,
-            "Puissance": 13,
-            "Qualité Physiques Naturelles": 16,
-            "Vitesse": 16
+// Fonction pour créer et ajouter un élément HTML avec des attributs
+function createElement(tag, attributes, text) {
+    const element = document.createElement(tag);
+    if (attributes) {
+        for (const key in attributes) {
+            element.setAttribute(key, attributes[key]);
         }
     }
-];
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+}
 
-const playerCardContainer = document.getElementById('player-card');
-playersData.forEach(playerData => {
-    const playerCard = createPlayerCard(playerData);
-    playerCardContainer.appendChild(playerCard);
-});
+// Fonction pour ajouter un joueur au document
+function addPlayerToDocument(player) {
+    const playersList = document.getElementById('players-list');
+    const playerCard = createElement('div', { class: 'player-card' });
+    const playerImage = createElement('img', { src: player.photo, alt: player.nom });
+    const playerName = createElement('h2', null, player.nom);
+    const attributesSection = createElement('div', { class: 'attributes-section' });
+
+    // Crée les tables pour les attributs techniques, mentaux et physiques
+    for (const category in player) {
+        if (category !== 'photo' && category !== 'nom') {
+            const attributesTable = createElement('div', { class: 'attributes-table' });
+            const tableTitle = createElement('h3', null, `Attributs ${category.charAt(0).toUpperCase() + category.slice(1)}`);
+            const attributeTable = createElement('table');
+            const tableBody = createElement('tbody');
+
+            for (const attribute in player[category]) {
+                const attributeRow = createElement('tr');
+                const attributeName = createElement('td', null, attribute);
+                const attributeValue = createElement('td', null, player[category][attribute]);
+
+                attributeRow.appendChild(attributeName);
+                attributeRow.appendChild(attributeValue);
+                tableBody.appendChild(attributeRow);
+            }
+
+            attributeTable.appendChild(tableBody);
+            attributesTable.appendChild(tableTitle);
+            attributesTable.appendChild(attributeTable);
+            attributesSection.appendChild(attributesTable);
+        }
+    }
+
+    playerCard.appendChild(playerImage);
+    playerCard.appendChild(playerName);
+    playerCard.appendChild(attributesSection);
+    playersList.appendChild(playerCard);
+}
+
+// Fonction pour charger les données des joueurs depuis un fichier JSON
+function loadPlayers() {
+    fetch('players.json')
+        .then(response => response.json())
+        .then(data => {
+            for (const player of data) {
+                addPlayerToDocument(player);
+            }
+        })
+        .catch(error => console.error('Erreur lors du chargement des joueurs :', error));
+}
+
+// Charge les données des joueurs lorsque la page est chargée
+window.addEventListener('load', loadPlayers);
+
 
 const backgrounds = ['img/wallpaper-01.jpg', 'img/wallpaper-02.jpg', 'img/wallpaper-05.jpg'];
 let currentBackgroundIndex = 0;
